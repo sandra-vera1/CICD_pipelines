92a38ce560ef3139291c4fe847df6a5c1005441e
sandra-vera1 <sandra-vera1@hotmail.com>
2025-03-20 16:25:45 -0600

    Changed text in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Enterprise Computing Assignment 2 by Sandra M Vera G/i);
+  const linkElement = screen.getByText(/TECH 2102 Enterprise Computing - Assignment 2 by Sandra Vera/i);
   expect(linkElement).toBeInTheDocument();
 });
